b75f7f71d559f32aef30ad96040d3f40cb6060f5
Tony Yao <Tony Yao@Tony>
2017-03-15 21:44:30 +0800

    Tony:编写了启动fx的方法，并初步完成了player的主界面（参考网易云音乐），现在能够运行主类查看页面的效果。但现在还没有方法接口，并且也不美观，过阵子再补上方法接口，并按照网易云音乐的模板追加主题色和图标。

---
+++ src/com/example/Main/Main.java
@@ -0,0 +1,36 @@
+package com.example.Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application{
+	/**
+	 * 用于开始运行fx程序的主函数
+	 * @author Tony Yao
+	 */
+	public static void main(String[] args) {  
+        Application.launch(Main.class, args);  
+    }  
+      
+	
+	/**
+	 * 借用fxml启动的fx程序
+	 * @author Tony Yao
+	 */
+    @Override  
+    public void start(Stage stage) throws Exception { 
+    	//装载fxml于fx主页面，fxml与当前类同一目录，将来可改位置
+        Parent root = FXMLLoader.load(getClass().getResource("MusicPlayMain.fxml"));  
+        
+        //初始化舞台，启动fxml，目前还不能操作只有界面而已，驱动方法接口过几天再补充
+        Scene scene = new Scene(root, 1300, 850);  
+        stage.initStyle(StageStyle.DECORATED);  
+        stage.setScene(scene);  
+        stage.setTitle("MusicPlay");  
+        stage.show();
+    }  
+}
