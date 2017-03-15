1b9c4d2d989c44a453ac0da82c24c5b241b88714
EnTaroAdunZ <635747298@qq.com>
2017-03-15 13:30:43 +0800

    上传实验

--- src/com/example/entity/TagMsg.java
+++ src/com/example/entity/TagMsg.java
@@ -4,7 +4,7 @@ package com.example.entity;
 * @author ZTF  
 * @date 2017年3月15日 下午1:19:24 
 * @version 1.0 
-* @Description:   TODO(用一句话描述该文件做什么)
+* @Description:   TAG解析实体
 */
 public class TagMsg {
 
