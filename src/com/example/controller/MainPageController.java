b6be38a7ea605af9f279cc7754d36f0c169adc27
Tony Yao <Tony Yao@Tony>
2017-03-18 01:35:24 +0800

    Tony:添加了搜索页面fxml（未实现切换）,添加了fxml里所有控件包括pane的id，修改了控制类的位置

--- src/com/example/controller/MainPageController.java
+++ src/com/example/controller/MainPageController.java
@@ -1,8 +1,18 @@
-package com.example.Main;
+package com.example.controller;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.example.Main.Main;
+import com.example.Main.MusicUtils;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -12,6 +22,10 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * @author Tony Yao
@@ -51,10 +65,22 @@ public class MainPageController {
 	private Button Button_addLocalDirectory;//添加本地音乐文件夹按钮id
 	
 	@FXML
+	private Button Button_back;//“后退”按钮的id
+	
+	@FXML
+	private Button Button_forward;//“前进”按钮的id
+	
+	@FXML
 	private Slider Slider_songProgress;//当前播放歌曲的进度条id
 	
 	@FXML
 	private Slider Slider_volumn;//当前播放歌曲的音量条id
+	
+	@FXML
+	private Label Label_FXName;//左上方该程序的名字；
+	
+	@FXML
+	private Label Label_musicListTitle;//左侧歌单栏栏头
 	
 	@FXML
 	private Label Label_currentTime;//当前播放歌曲的当前播放时间id
@@ -104,6 +130,55 @@ public class MainPageController {
 	@FXML
 	private ScrollBar ScrollBar_music;//歌曲总表的拖动条id
 	
+	//以下是不太会用到的pane的id，以防万一会使用到
+	@FXML
+	private BorderPane BorderPane_mainPage;//主界面的底层paneid
+	
+	@FXML
+	private AnchorPane AnchorPane_top;//顶层部分的区域id（标题栏区域）
+	
+	@FXML
+	private AnchorPane AnchorPane_left;//左边部分的区域id（歌单区域）
+	
+	@FXML
+	private AnchorPane AnchorPane_center;//中间部分的区域id（歌表区域）
+	
+	@FXML
+	private AnchorPane AnchorPane_bottom;//底层部分的区域id（播放条区域）
+	
+	@FXML
+	private AnchorPane AnchorPane_CurrentSongInfo;//左下当前播放歌曲的信息区域id
+	
+	@FXML
+	private HBox Hbox_topItemHbox;//控制顶层部件横向排列的id
+	
+	@FXML
+	private HBox HBox_leftButtonAndScroll;//控制左侧按钮们和滚动条横向排列的id
+	
+	@FXML 
+	private HBox HBox_leftManageField;//控制“我的歌单”Label和“添加歌单”按钮横向排列的id
+	
+	@FXML
+	private HBox Hbox_leftImageAndTitle;//控制左下专辑图片和标题歌手横向排列的id
+	
+	@FXML
+	private HBox HBox_centerMusicListAndScroll;//控制歌表和滚动条横向排列的id
+	
+	@FXML
+	private HBox HBox_bottomItems;//控制底层部件横向排列的id
+	
+	@FXML
+	private VBox VBox_leftItems;//控制左侧部件竖向排列的id
+	
+	@FXML
+	private VBox VBox_leftMainField;//控制左侧按钮们和“我的歌单”竖向排列的id
+
+	@FXML
+	private VBox VBox_leftButtons;//控制左侧按钮们竖向排列的id
+	
+	@FXML
+	private VBox Vbox_leftTitleAndSinger;//控制左侧当前播放音乐标题和歌手竖向排列的id
+	
 	//以下是部分控件的使用方法id
 	@FXML
 	private void onButtonLast(ActionEvent event){ //按下“上一首”按钮的接口方法，待实现
@@ -131,7 +206,7 @@ public class MainPageController {
 	}
 	
 	@FXML
-	private void onButtonSearch(ActionEvent event){ //按下“search”按钮的接口方法，待实现
+	private void onButtonSearch(ActionEvent event) { //按下“search”按钮的接口方法，待实现
 		
 	}
 	
@@ -147,6 +222,16 @@ public class MainPageController {
 	
 	@FXML
 	private void onAddLocalDirectory(ActionEvent event){//按下“添加本地音乐文件夹”按钮的接口方法，待实现
+		
+	}
+	
+	@FXML
+	private void onBack(ActionEvent event){//按下“后退”按钮的接口方法，待实现
+		
+	}
+	
+	@FXML
+	private void onForward(ActionEvent event){//按下“前进”按钮的接口方法，待实现
 		
 	}
 }
---
+++ src/com/example/controller/SearchPageController.java
@@ -0,0 +1,78 @@
+package com.example.controller;
+
+import com.example.Main.MusicUtils;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * @author Tony Yao
+ * 该类是fxml文件SearchPage.fxml 的控制类，用于声明各控件的id和方法接口
+ */
+public class SearchPageController {
+	@FXML
+	private AnchorPane AnchorPane_searchPage;//整个搜索页面的底层pane
+	
+	@FXML
+	private VBox VBox_vbox;//搜索页面的排列框
+	
+	@FXML
+	private Label Label_searchResult;//搜索页面里显示搜索简略信息的文本Label的Id
+	
+	@FXML
+	private TabPane TabPane_tabPane;//搜索页面的标签栏pane的id
+	
+	@FXML
+	private Tab Tab_singleMusic;//“单曲”标签的id
+	
+	@FXML
+	private Tab Tab_musicSinger;//“歌手”标签的id
+	
+	@FXML
+	private Tab Tab_album;//“专辑”标签的id
+	
+	@FXML
+	private Tab Tab_musicList;//“歌单”标签的id
+	
+	@FXML
+	private Tab Tab_musicLyrics;//“歌词”标签的id
+	
+	@FXML
+	private TableView<MusicUtils> TableView_searchMusicPage;//单曲栏下的歌表id
+	
+	@FXML
+	private TableColumn<MusicUtils, String> TableColumn_search_ID;//歌表中隐藏的“ID”栏的id
+	
+	@FXML
+	private TableColumn<MusicUtils, String> TableColumn_search_musicTitle;//歌表中的“音乐标题”栏的id
+	
+	@FXML
+	private TableColumn<MusicUtils, String> TableColumn_search_singer;//歌表中的“歌手”栏的id
+	
+	@FXML
+	private TableColumn<MusicUtils, String> TableColumn_search_albumName;//歌表中的“专辑”栏的id
+
+	@FXML
+	private TableColumn<MusicUtils, String> TableColumn_search_timeLength;//歌表中的“时长”栏的id
+	
+	@FXML
+	private ListView<Button> ListView_SingerPage;//歌手栏下的按钮列表id
+
+	@FXML
+	private ListView<Button> ListView_albumPage;//专辑栏下的按钮列表id
+
+	@FXML
+	private ListView<Button> ListView_musicListPage;//歌单栏下的按钮列表id
+
+	@FXML
+	private ListView<Button> ListView_lyricsPage;//歌词栏下的按钮列表id
+	
+}
